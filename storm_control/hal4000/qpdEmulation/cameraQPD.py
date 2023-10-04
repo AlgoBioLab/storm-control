c14d80d739cf4dfd231142fe3ab6133741c6def7
cbolles <collinbolles@gmail.com>
2023-10-04 15:59:54 -0400

    Begin copying over Scipy fit

--- storm_control/hal4000/qpdEmulation/cameraQPD.py
+++ storm_control/hal4000/qpdEmulation/cameraQPD.py
@@ -5,6 +5,7 @@ from storm_control.hal4000.halLib.halFunctionality import HalFunctionality
 import storm_control.sc_hardware.baseClasses.hardwareModule as hardwareModule
 import storm_control.sc_hardware.baseClasses.lockModule as lockModule
 import storm_control.hal4000.halLib.halMessage as halMessage
+import storm_control.sc_hardware.utility.np_lock_peak_finder as npLPF
 
 
 class Camera(hardwareModule.HardwareModule, HalFunctionality): # metaclass=abc.ABCMeta):
@@ -63,7 +64,7 @@ class Camera(hardwareModule.HardwareModule, HalFunctionality): # metaclass=abc.A
 
 
 
-class CameraQPD(hardwareModule.HardwareModule): # lockModule.QPDCameraFunctionalityMixin):
+class CameraQPD(hardwareModule.HardwareModule, lockModule.QPDCameraFunctionalityMixin, metaclass=abc.ABCMeta):
     CAMERA_MODULE_ERROR = 'Configuration has not completed successfully, camera for QPD emulation not found'
 
     def __init__(self, module_params = None, qt_settings = None, **kwds):
@@ -114,3 +115,76 @@ class CameraQPD(hardwareModule.HardwareModule): # lockModule.QPDCameraFunctional
         if (message.getData()["name"] == self.module_name):
             message.addResponse(halMessage.HalMessageResponse(source = self.module_name,
                                                               data = {"functionality" : self}))
+
+    @abc.abstractmethod
+    def doFit(self, data: np.ndarray) -> Tuple:
+        """ Handle the fit calculations on the image data """
+        pass
+
+
+class CameraQPDFit(metaclass=abc.ABCMeta):
+    """ Interface which descibes how fit implementations function """
+    @abc.abstractmethod
+    def doFit(self, data: np.ndarray) -> Tuple:
+        pass
+
+
+class CameraQPDScipyFit(CameraQPDFit):
+    """
+    QPD Fit based on leveraging scipy. This implementation can be originally
+    found in
+    `storm_control/sc_hardware/thorlabs/uc480Camera.py`
+    """
+    def __init__(self, fit_mutex = None, **kwds):
+        super().__init__(**kwds)
+
+        self.fit_mutex = fit_mutex
+
+    def doFit(self, data) -> Tuple:
+        dist1 = 0
+        dist2 = 0
+        self.x_off1 = 0.0
+        self.y_off1 = 0.0
+        self.x_off2 = 0.0
+        self.y_off2 = 0.0
+
+        # numpy finder/fitter.
+        #
+        # Fit first gaussian to data in the left half of the picture.
+        total_good =0
+        [max_x, max_y, params, status] = self.fitGaussian(data[:,:self.half_x])
+        if status:
+            total_good += 1
+            self.x_off1 = float(max_x) + params[2] - self.half_y
+            self.y_off1 = float(max_y) + params[3] - self.half_x
+            dist1 = abs(self.y_off1)
+
+        # Fit second gaussian to data in the right half of the picture.
+        [max_x, max_y, params, status] = self.fitGaussian(data[:,-self.half_x:])
+        if status:
+            total_good += 1
+            self.x_off2 = float(max_x) + params[2] - self.half_y
+            self.y_off2 = float(max_y) + params[3]
+            dist2 = abs(self.y_off2)
+
+        return (total_good, dist1, dist2)
+
+    def fitGaussian(self, data):
+        if (np.max(data) < 25):
+            return [False, False, False, False]
+        x_width = data.shape[0]
+        y_width = data.shape[1]
+        max_i = data.argmax()
+        max_x = int(max_i/y_width)
+        max_y = int(max_i%y_width)
+        if (max_x > (self.fit_size-1)) and (max_x < (x_width - self.fit_size)) and (max_y > (self.fit_size-1)) and (max_y < (y_width - self.fit_size)):
+            if self.fit_mutex is not None:
+                self.fit_mutex.lock()
+            [params, status] = npLPF.fitFixedEllipticalGaussian(data[max_x-self.fit_size:max_x+self.fit_size,max_y-self.fit_size:max_y+self.fit_size], self.sigma)
+            if self.fit_mutex:
+                self.fit_mutex.unlock()
+            params[2] -= self.fit_size
+            params[3] -= self.fit_size
+            return [max_x, max_y, params, status]
+        else:
+            return [False, False, False, False]
