f48204328df289c256c95b8355cde391fdc823e9
cbolles <collinbolles@gmail.com>
2023-10-11 11:56:39 -0400

    Make emulator for QPD camera

--- storm_control/hal4000/qpdEmulation/cameraInterface.py
+++ storm_control/hal4000/qpdEmulation/cameraInterface.py
@@ -60,3 +60,66 @@ class Camera(hardwareModule.HardwareModule, HalFunctionality): # metaclass=abc.A
     def processMessage(self, message):
         if message.isType('get functionality') and message.getData()['name'] == self.module_name:
             message.addResponse(halMessage.HalMessageResponse(source=self.module_name, data={'functionality': self}))
+
+
+
+class CameraNone(Camera):
+    """
+    Implementation that will return two dots at a fixed distance apart
+    """
+    import cv2
+
+    def __init__(self, module_params = None, qt_settings = None, **kwds):
+        super().__init__(**kwds)
+        assert module_params is not None
+
+        self.timeout = 0.5
+
+        # The actual AOI is not important in the camera emulation, the size
+        # of the AOI is needed to generate the image
+        self.aoi = (0, 0, 200, 200)
+        self.dimensions = (200, 200)
+
+        configuration = module_params.get('configuration')
+        self.circle_radius = configuration.get('circule_radius', 5)
+        self.circle_shade = configuration.get('circle_shade', 200)
+        self.sigma = configuration.get('sigma', 5)
+
+
+    def getImage(self) -> np.ndarray:
+        """
+        Create an image with two gaussian blurs
+        """
+        # Make a black image
+        image = np.full(self.dimensions, 0, dtype=np.uint8)
+
+        # Make left circle
+        left_center = (int(0.25 * self.dimensions[1]), int(0.5 * self.dimensions[0]))
+        image = CameraNone.cv2.circle(image, left_center, self.circle_radius, (self.circle_shade,), -1)
+
+        # Make right circle
+        right_center = (int(0.75 * self.dimensions[1]), int(0.5 * self.dimensions[0]))
+        image = CameraNone.cv2.circle(image, right_center, self.circle_radius, (self.circle_shade,), -1)
+
+        # Blur the image
+        image = CameraNone.cv2.GaussianBlur(image, (self.sigma, self.sigma), 0)
+
+        return image
+
+    def getTimeout(self) -> float:
+        return self.timeout
+
+    def setTimeout(self, timeout: float) -> None:
+        self.timeout = timeout
+
+
+    def getAOI(self):
+        return self.aoi
+
+    def setAOI(self, x_start: int, y_start: int, width: int, height: int) -> None:
+        self.aoi = (x_start, y_start, width, height)
+        self.dimensions = (height, width)
+
+    def shutdown(self):
+        pass
+
